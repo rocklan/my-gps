870104642e1223364004e96cec9d5b5a24be0df0
Lachlan Barclay <lachlan.barclay@mercer.com>
2020-08-04 16:29:14 +1000

    first commit

---
+++ gps.js
@@ -0,0 +1,185 @@
+(function () {
+
+	var gpsData = [];
+	var tracking = false;
+	var rows = "";
+	var lastLoc = null;
+	var dist = 0;
+	var mymap;
+
+	startup();
+
+	function startup() {
+
+		setupEventHandlers();
+
+		setupMap();
+
+		trackStuff();
+
+		setInterval(function () {
+			trackStuff();
+		}, 5000);
+	}
+
+	function setupMap() {
+		navigator.geolocation.getCurrentPosition(function (location) {
+
+			mymap = L.map('mapid').setView([location.coords.latitude, location.coords.longitude], 12);
+
+			L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+				maxZoom: 18,
+				id: 'mapbox/streets-v11',
+				tileSize: 512,
+				zoomOffset: -1,
+				accessToken: 'pk.eyJ1Ijoicm9ja2xhbiIsImEiOiJja2RmamNyeTM0eWlsMnVxeWZ0cHhiNHZ6In0.VFVX9RunPHmBt8_NeJ2Ppw'
+			}).addTo(mymap);
+
+			var circle = L.circle([location.coords.latitude, location.coords.longitude], {
+				color: 'red',
+				fillColor: '#f03',
+				fillOpacity: 0.25,
+				radius: 2500
+			}).addTo(mymap);
+		});
+
+	}
+
+	function setupEventHandlers() {
+		var startButton = document.getElementById('Start');
+		var stopButton = document.getElementById('Stop');
+		var downloadButton = document.getElementById('Download');
+		var clearButton = document.getElementById('Clear');
+
+		startButton.addEventListener("click", function () {
+			tracking = true;
+		});
+		stopButton.addEventListener("click", function () {
+			tracking = false;
+		});
+		downloadButton.addEventListener("click", function () {
+			downloadData();
+		});
+		clearButton.addEventListener("click", function () {
+			clear();
+		});
+	}
+
+	function clear() {
+		rows = "";
+		document.getElementById("tabledetails").innerHTML = rows;
+	}
+
+	function trackStuff() {
+
+		if (tracking) {
+			navigator.geolocation.getCurrentPosition(function (location) {
+
+				var tooClose = false;
+
+				if (lastLoc) {
+					dist = distanceInKm(
+						lastLoc.coords.latitude,
+						lastLoc.coords.longitude,
+						location.coords.latitude,
+						location.coords.longitude);
+
+					if (dist < 0.010)
+						tooClose = true;
+				}
+
+				if (!tooClose) {
+					lastLoc = location;
+
+					gpsData.push({
+						lat: location.coords.latitude,
+						lon: location.coords.longitude,
+						ele: location.coords.altitude,
+						tim: new Date(location.timestamp)
+					});
+				}
+
+				var row = "<tr><td>" + new Date(location.timestamp).toLocaleTimeString() +
+					"</td><td>" + location.coords.latitude +
+					"</td><td>" + location.coords.longitude +
+					"</td><td>" + location.coords.altitude +
+					"</td><td>" + location.coords.accuracy +
+					"</td><td>" + (dist * 1000);
+
+				if (tooClose)
+					row += " ignored (too close)";
+
+				row += "</td></tr>";
+
+				rows += row;
+
+				document.getElementById("tabledetails").innerHTML = rows;
+			});
+
+		}
+	}
+
+	function degreesToRadians(degrees) {
+		return degrees * Math.PI / 180;
+	}
+
+	function distanceInKm(lat1, lon1, lat2, lon2) {
+		var earthRadiusKm = 6371;
+
+		var dLat = degreesToRadians(lat2 - lat1);
+		var dLon = degreesToRadians(lon2 - lon1);
+
+		lat1 = degreesToRadians(lat1);
+		lat2 = degreesToRadians(lat2);
+
+		var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		return earthRadiusKm * c;
+	}
+
+	function downloadData() {
+
+		var fileContents = "";
+		var trk;
+
+		for (var i = 0; i < gpsData.length; i++) {
+			trk = gpsData[i];
+
+			fileContents += '<trkpt lat="' + trk.lat + '" lon="' + trk.lon + '">';
+
+			if (trk.ele)
+				fileContents += '<ele>' + trk.ele + '</ele>';
+
+			fileContents += '<time>' + trk.tim.toISOString() + '</time></trkpt>';
+
+		}
+
+		var outputFile =
+			'<gpx version="1.1" creator="lachlanbarclaynet">' +
+			'<trk>' +
+			'<name>lachs walk</name>' +
+			'<trkseg>' + fileContents + '</trkseg>' +
+			'</trk>' +
+			'</gpx>';
+
+		download('walk.gpx', outputFile);
+
+	}
+
+	function download(filename, text) {
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+		element.setAttribute('download', filename);
+
+		element.style.display = 'none';
+		document.body.appendChild(element);
+
+		element.click();
+
+		document.body.removeChild(element);
+	}
+
+})();
+
